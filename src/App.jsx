49fc8d7b0c961e7bd25df40d8846d369806033ba
Graeme Byrne <grmbrn89@gmail.com>
2024-12-02 21:00:22 +0100

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,16 @@
+import Hero from "./components/hero/Hero"
+import Services from "./components/services/Services"
+import Contact from "./components/contact/Contact"
+import Portfolio from "./components/portfolio/Portfolio"
+
 const App = () => {
   return (
-    <div className=''>Hello World</div>
+    <div className=''>
+      <Hero />
+      <Services />
+      <Portfolio />
+      <Contact />
+    </div>
   )
 }
 
